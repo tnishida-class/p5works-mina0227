75b1e498d898f238efb10dc0c4a3dd4b1d64235b
mina <1910512h@stu.kobe-u.ac.jp>
2020-10-20 12:01:20 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,17 @@
 // 小手調べ
 function setup(){
+  createCanvas(400,400);
+  fill(0);
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    let x=20*i;
+    if(i<5){
+    stroke(0,0,255);
+    noFill(0)
+    ellipse(200,200,x);
+    }
+    else{
+    stroke(255,0,0);
+    ellipse(200,200,x)
+    }
   }
 }
