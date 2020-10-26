9c5ea722b2b7b5bb7a4e5fe8f67859466e63e137
mina <1910512h@stu.kobe-u.ac.jp>
2020-10-26 11:19:33 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -8,17 +8,17 @@ function setup(){
      fill(255,255,255)}
       else if(i%2==1&&j%2==1){
      fill(255,255,255)}
-      else{fill(128,128,128)}
-      rect(i * size, j * size, 30,30);
+      else{fill(128)}
+      rect(i * size, j * size, 50,50);
       if(i%2==1&&j%2==0&&j<3){
      fill(255,0,0)}
       else if(i%2==0&&j%2==1&&j<3){ fill(255,0,0)}
-
       else if(i%2==1&&j%2==0&&j>4&&j<9){
      fill(0)}
       else  if(i%2==0&&j%2==1&&j>4&&j<9){ fill(0)}
+
   ellipse(i*size+15,j*size+15,25)
-    }
+         }
 
 }
 }
