2c52f4675e9fe1254b5e4c12c4ec6e0ad945850f
mina <1910512h@stu.kobe-u.ac.jp>
2020-10-26 14:50:01 +0900

    Update sketch.js

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -9,13 +9,18 @@ function setup() {
 
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+    if(i%2==0){
+      fill(blue)}
+    else if(i%2==1){
+      fill(255)}
     rect(0, i * d, width, (i + 1) * d);
   }
-
   fill(blue);
   let size = d * 5;
   rect(0, 0, size, size);
-
   fill(255);
+  noStroke();
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(30,0,20,size)
+  rect(0,2*d,size,20)
 }
