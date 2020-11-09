a7534a9ec2e4f38cfc7e64f937c6a72a777cd997
mina <1910512h@stu.kobe-u.ac.jp>
2020-11-09 23:38:57 +0900

    Update sketch.js

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,14 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("Kobe university",255,255,255,0,255,0,30);
 }
 
-function balloon(t){
+function balloon(t,a,b,c,x,y,z,u){
   let w = textWidth(t);
-  let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
+  let h = textAscent() + textDescent();//文字の高さ
+  let p = u;//余白の大きさ]
+  fill(x,y,z)
   rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
+ fill(a,b,c)
   text(t, p, h + p);
-}
