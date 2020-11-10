ebad4fe06f4fd94d98456e500e6410de8fa8292a
mina <1910512h@stu.kobe-u.ac.jp>
2020-11-10 11:07:26 +0900

    Update sketch.js

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,14 +4,16 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("Kobe university",255,255,255,0,255,0,30);
+  balloon("Kobe university",color(255,255,255),color(0,255,0),30);
 }
 
-function balloon(t,a,b,c,x,y,z,u){
+function balloon(t,color1,color2,u){
   let w = textWidth(t);
   let h = textAscent() + textDescent();//文字の高さ
   let p = u;//余白の大きさ]
-  fill(x,y,z)
+  fill(color1)
   rect(0, 0, w + p * 2, h + p * 2);
- fill(a,b,c)
+ fill(color2)
   text(t, p, h + p);
+}
+  
