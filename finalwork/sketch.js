b3781fa8ef20dc61b5ac0643b40f953878f8c1c9
mina <1910512h@stu.kobe-u.ac.jp>
2020-11-22 22:46:15 +0900

    Update sketch.js

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,105 @@
-// 最終課題を制作しよう
 
-function setup(){
-  createCanvas(windowWidth, windowHeight);
+
+function updatePosition(entity) {
+  entity.x += entity.vx;
+  entity.y += entity.vy;
 }
 
-function draw(){
-  background(160, 192, 255);
+
+function createPlayer() {
+  return {
+    x: 300,
+    y: 300,
+    vx: 0,
+    vy: 0
+  };
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+function applyGravity(entity) {
+  entity.vy += 0.2;
 }
+
+function applyJump(entity) {
+  entity.vy = -5;
+}
+
+function drawPlayer(entity) {
+  push()
+  noStroke()
+ fill("#31326f")
+  ellipse(entity.x, entity.y, 45);
+  pop()
+
+}
+
+
+
+function createBlock(y) {
+  return {
+    x: 900,
+    y,
+    vx: -2,
+    vy: 0
+  };
+}
+
+function drawBlock(entity) {
+  push()
+  noStroke()
+  fill("#FFDADA")
+  rect(entity.x, entity.y, 80, 300);
+  pop()
+}
+
+function blockIsAlive(entity) {
+
+  return -100 < entity.x;
+}
+
+
+let player;
+
+let blocks;
+
+function addBlocks() {
+  let y = random(-100, 100);
+  blocks.push(createBlock(y + 350));
+}
+
+
+ function setup() {
+  createCanvas(800, 600);
+  rectMode(CENTER);
+
+
+  player = createPlayer();
+
+  blocks = [];
+}
+
+function draw() {
+  if (frameCount % 140 === 1) addBlocks(blocks);
+  blocks = blocks.filter(blockIsAlive);
+
+
+  updatePosition(player);
+  for (let block of blocks) updatePosition(block);
+
+
+  applyGravity(player);
+
+  background("#dbf6e9");
+  drawPlayer(player);
+  for (let block of blocks) drawBlock(block);
+  noStroke()
+  fill(220)
+  rect(400,600,800,400)
+}
+
+function mousePressed() {
+
+  applyJump(player);
+
+
+  }
+  
